70c270ce88302fd142e64691e196b0ca3dc3e173
Ronnie Rios <ronniesmail14@gmail.com>
2021-04-18 20:53:20 -0500

    questions and answers can display properly and step through question array

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,15 @@
-//start button
+//elements
 const startButton = document.querySelector("#start-btn")
 const questionContainerEl = document.querySelector("#question-container")
-let randomqQuestions, currentQuestion
 
-let questionIndex = 0
 //question and andswer elements
 const questionElement = document.querySelector("#question")
 const answerElement = document.querySelector("#answer-buttons")
 //time left
 var timeLeft = 60;
-//answer variables
-const choice1 = document.getElementById("#btn-1")
-const choice2 = document.getElementById("#btn-2")
-const choice3 = document.getElementById("#btn-3")
-const choice4 = document.getElementById("#btn-4")
+let randomqQuestions, currentQuestion
+
+let questionIndex = 0
 //question array
 const questionArr =[
     {
@@ -27,14 +23,18 @@ const questionArr =[
     choices: ['array.loop', 'If statement', 'For Loop', 'prompt'],
     correctAnswer: 'For Loop'
     },
+
+    {
+    question: "Booleans have what two values",
+    choices: ['true or false', '1 or 0', 'string or number', 'prompt or function'],
+    correctAnswer: 'true or false'
+    },
+   
 ]
 
 //startgame function
 function startGame() {
-   
-    //startButton.style.display = "none";
     document.querySelector("#start-control").setAttribute("class", "hide");
-    // randomqQuestions= questionArr.sort(() => Math.random() -.5);
     currentQuestion = 0;
     questionContainerEl.classList.remove('questions-hide')
     setNextQuestion()
@@ -42,38 +42,61 @@ function startGame() {
 
 //next question
 function setNextQuestion() {
-    resetDisplay()
     displayQuestion(questionArr[questionIndex])
+   
 }
 
 //dynamically displays questions and answers from objects in questionArr
 function displayQuestion(question) {
     //display question
     questionElement.innerText = question.question
-    var choices = question.choices
+    // var answerChoices = question.choices
     //for loop to append answer choices and buttons from questionArr
-    for(var i=0;i<choices.length;i++){
-        var answerBtn=document.createElement('button');
-        var buttonName = choices[i]
-        answerBtn.setAttribute('class','btn')
-        answerBtn.setAttribute('id', 'btn-' + (i+1));
-        answerBtn.setAttribute("value", buttonName)
-        answerBtn.innerHTML = buttonName
-        answerElement.appendChild(answerBtn);
-        answerBtn.onclick = checkAnswer;
-    }
+    // for(var i=0;i<choices.length;i++){
+    //     var answerBtn=document.createElement('button');
+    //     var buttonName = choices[i]
+    //     answerBtn.setAttribute('class','btn')
+    //     answerBtn.setAttribute('id', 'btn-' + (i+1));
+    //     answerBtn.setAttribute("value", buttonName)
+    //     answerBtn.innerHTML = buttonName
+    //     answerElement.appendChild(answerBtn);
+    //     answerBtn.onclick = checkAnswer;
+    //       }
+    var answer1 =
+    "<button id='choice1' class='btn btn-primary btn-lg btn-block answerChoice'>" +
+    questionArr[questionIndex].choices[0];
+    var answer2 =
+        "<button id='choice2' class='btn btn-primary btn-lg btn-block answerChoice'>" +
+        questionArr[questionIndex].choices[1];
+    var answer3 =
+        "<button id='choice3' class='btn btn-primary btn-lg btn-block answerChoice'>" +
+        questionArr[questionIndex].choices[2];
+    var answer4 =
+        "<button id='choice4' class='btn btn-primary btn-lg btn-block answerChoice'>" +
+        questionArr[questionIndex].choices[3];
+
+  answerElement.innerHTML =
+    "<ul class='no-bullets'><li>" +
+    answer1 +
+    "</li><li>" +
+    answer2 +
+    "</li><li>" +
+    answer3 +
+    "</li><li>" +
+    answer4 +
+    "</li></ul>";
 }
 
 //timer function to display timer and start countdown
-function countdown() {
-    var timeleft = 60;
-    var timeInterval = setInterval(function() {
-        if (timeLeft > 1) {
-            console.log(hello)
-            timeLeft--
-        }
-    })
-}
+// function countdown() {
+//     var timeleft = 60;
+//     var timeInterval = setInterval(function() {
+//         if (timeLeft > 1) {
+//             console.log(hello)
+//             timeLeft--
+//         }
+//     })
+// }
 //gets user button click and checks it to see if it's correct
 function checkAnswer(e) {
     var checkedAnswer = questionArr[questionIndex].correctAnswer
@@ -95,3 +118,4 @@ function checkAnswer(e) {
 
 //event listeners
 startButton.addEventListener('click', startGame)
+answerElement.addEventListener('click', checkAnswer)
