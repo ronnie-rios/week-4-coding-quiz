470b27f270d96fbfaf1a05303884746aa24f5f75
Ronnie Rios <ronniesmail14@gmail.com>
2021-04-19 00:49:20 -0500

    added ability for user to enter intials at end

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,17 @@
-//elements
+//container and div elements
 const startButton = document.querySelector("#start-btn")
 const questionContainerEl = document.querySelector("#question-container")
 const startingElement = document.querySelector("#start-control")
+const resultEl = document.querySelector("#result")
 
-//question and andswer elements
+//question and answer elements
 const questionElement = document.querySelector("#question")
 const answerElement = document.querySelector("#answer-buttons")
 //time left
 var timeLeft = 60;
+//user score
 let score = 0
-
+//keep track of questions
 let questionIndex = 0
 
 //question array
@@ -21,19 +23,27 @@ const questionArr =[
     },
 
     {
-    question: "Which of the following would you use to iterate though an array",
+    question: "Which of the following would you use to iterate though an array?",
     choices: ['array.loop', 'If statement', 'For Loop', 'prompt'],
     correctAnswer: 'choice3'
     },
 
     {
-    question: "Booleans have what two values",
+    question: "Booleans have what two values?",
     choices: ['true or false', '1 or 0', 'string or number', 'prompt or function'],
     correctAnswer: 'choice1'
     },
-   
+    
+    {
+    question: "How would you display 'Hello World' in an alert box?",
+    choices: ['prompt()', 'console.log', 'box()', 'alert()'],
+    correctAnswer: 'choice4'
+    },
 ]
-var lastQuestion = 3
+var lastQuestion = 4
+
+//userscore arr
+var scoreArr = []
 //startgame function
 function startGame() {
     document.querySelector("#start-control").setAttribute("class", "hide");
@@ -47,7 +57,7 @@ function startGame() {
 //next question
 function setNextQuestion() {
     displayQuestion(questionArr[questionIndex])
-  
+   
 }
 
 //dynamically displays questions and answers from objects in questionArr
@@ -87,8 +97,9 @@ function countDown(){
             timeLeft=0;
         }
     document.getElementById('countdown').innerHTML = 'Time: ' + timeLeft;
-    if(timeLeft <= 0 || questionIndex == 3){
+    if(timeLeft <= 0 || questionIndex == 4){
         clearInterval(timer);
+        userScore();
     }
     timeLeft--;
 }, 1000);
@@ -96,7 +107,7 @@ function countDown(){
 //gets user button click and checks it to see if it's correct
 function checkAnswer(e) {
     var checkedAnswer = questionArr[questionIndex].correctAnswer
-    
+    //gets user button click to compare answers
     var userAnswer =e.target.id
    if (userAnswer === checkedAnswer) {
        console.log ("hello, youre right");
@@ -107,24 +118,43 @@ function checkAnswer(e) {
       timeLeft = timeLeft - 10;
       score -= 5;
    }
-
+   //move on to next question
     questionIndex++
     console.log(score);
-    
+    //way to end game
    if (questionIndex == lastQuestion){
        userScore();
+    //    if (button.id === "submitBtn") {
+    //     var savedInitials = document.getElementById("initials").value
+    //     saveScore(savedInitials)
+    //    }
    } else {
        setNextQuestion();
    }}
    
-
+//show user their score
 function userScore() {
     questionElement.innerText ="Congratulations on finishing."
     answerElement.innerHTML = "Your final score is " + score + ".";
-   
+    var userInitials = "<input id='initials' class='btn' name='initials' placeholder-''/>"
+    var initialButton = '<button id="submitBtn">Submit<type="submit">'
+    resultEl.innerHTML = "Please enter your initials." + userInitials + initialButton;
 }
-//function to save score to local storage
+//function to save score to local storage incomplete
+// function saveScore() {
+//     var userScoreArr = {
+//         score: 0,
+//         initials: ''
+//     }
+    
+//     userScoreArr.score = score;
+//     userScoreArr.initials = savedInitials
+//     scoreArr.push(userScoreArr);
+//     console.log(scoreArr)
+//     localStorage.setItem("user-scores", JSON.stringify(scoreArr))
+// }
 
+   
 //funciton to display high schoe when click on 'view score'
 
 //event listeners
